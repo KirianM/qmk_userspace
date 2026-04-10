5f139bc3241ea06ea4743ba1f2906a49e77800dd
Kirian Murgadella <844565+KirianM@users.noreply.github.com>
2026-04-10 22:53:51 +0200

    Update tapping behavior and combo settings in config.h

--- keyboards/splitkb/aurora/corne/rev1/keymaps/default/config.h
+++ keyboards/splitkb/aurora/corne/rev1/keymaps/default/config.h
@@ -5,8 +5,16 @@
 
 #pragma once
 
+// --- Tapping behavior ---
 #define TAPPING_TERM 200
+#define TAPPING_TERM_PER_KEY
 #define HOLD_ON_OTHER_KEY_PRESS
 #define PERMISSIVE_HOLD
+
+// --- One-shot modifiers ---
 #define ONESHOT_TAP_TOGGLE 3
 #define ONESHOT_TIMEOUT 2000
+
+// --- Combos ---
+#define COMBO_COUNT 3
+#define COMBO_TERM 40

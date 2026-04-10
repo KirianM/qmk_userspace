403ae1d29f0ca3aec977a5c25d8c847ca42f9e33
Kirian Murgadella <844565+KirianM@users.noreply.github.com>
2026-04-10 22:42:33 +0200

    Add configuration for Corne keyboard

---
+++ keyboards/splitkb/aurora/corne/rev1/keymaps/default/config.h
@@ -0,0 +1,12 @@
+/*
+ * Corne keyboard configuration
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#pragma once
+
+#define TAPPING_TERM 200
+#define HOLD_ON_OTHER_KEY_PRESS
+#define PERMISSIVE_HOLD
+#define ONESHOT_TAP_TOGGLE 3
+#define ONESHOT_TIMEOUT 2000
